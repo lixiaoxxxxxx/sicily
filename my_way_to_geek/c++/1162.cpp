76fae61a298dfe28ffc0281424be153ca45adcd4
lixiao <lixiaoxxxxxx@gmail.com>
2012-10-09 21:34:15 +0800

    asd

--- my_way_to_geek/c++/1162.cpp
+++ my_way_to_geek/c++/1162.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
 #include <stdio.h>
 using namespace std;
+int depth;
+int sudoku[9][9];
+
+bool rowcheck(int row){
+	int check[9];
+	for (int i = 0; i < 9; i++){
+		check[sudoku[row][i] - 1] = 1;
+	}
+	for (int i = 0; i < 9; i++)
+		if (check[i] == 0)
+			return false;
+	return true;
+}
+
+bool colcheck(int col){
+	int check[9];
+	for (int i = 0; i < 9; i++){
+		check[sudoku[i][col] - 1] = 1;
+	}
+	for (int i = 0; i < 9; i++)
+		if (check[i] == 0)
+			return false;
+	return true;
+}
+
+bool blockscheck(int row, int col){
+
+
+void dfs(){
+
+
 int main(){
-	int sudoku[9][9];
+	int row[9][9];
+	int col[9][9];
 	int k;
 	char tempchar;
 	cin >> k;
@@ -11,9 +43,7 @@ int main(){
 			for (int j = 0; j < 9; j++){
 				cin >> tempchar;
 				sudoku[i][j] = tempchar - '0';
-				cout << sudoku[i][j];
 			}
-			cout << endl;
 		}
 	}
 	return 0;
